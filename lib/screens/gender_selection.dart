16b83b2413cef6bcc8e139f001dc39de7df22658
vish198910 <vish198910@gmail.com>
2020-04-24 12:33:49 +0530

    Dashboard and Login ui is made

---
+++ lib/screens/gender_selection.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class GenderSelection extends StatefulWidget {
+  @override
+  _GenderSelectionState createState() => _GenderSelectionState();
+}
+
+class _GenderSelectionState extends State<GenderSelection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        
+      ),
+    );
+  }
+}
